03cdf80d618245b86c69b939fdddb0c41af0fd81
Rashad <rashadjaraysa98@gmail.com>
2019-07-18 15:00:22 -0700

    Initial Commit

---
+++ ImageDescription/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageDescription
+//
+//  Created by Rashad Jaraysa on 7/18/19.
+//  Copyright © 2019 Rashad Jaraysa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
